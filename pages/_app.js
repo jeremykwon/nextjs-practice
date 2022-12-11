04bc78a92e3165e60a6481c957f33b88fbf27b22
jeremykwon <kwonhyunwoo@likelion.org>
2022-12-11 21:44:38 +0900

    useRouter, url mask, _app.js 기능 사용

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import Head from "next/head";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      {/* 사이트 타이틀 변경 */}
+      <Head>
+        <title>test</title>
+      </Head>
+      <h1>pages/_app 최상위 테스트</h1>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,44 @@
 // import Image from "next/image";
-import styles from "@/styles/Home.module.css";
 import Link from "next/link";
+import { useRouter } from "next/router";
+
+// component
+import { Header } from "@/components";
 
 export default function Home() {
+  const router = useRouter();
+  console.log(router);
+
+  const moveTestPage = () => {
+    // router.push("test", "mask-url-router");
+    router.push(
+      {
+        pathname: "test",
+        query: { title: "test-id" },
+      },
+      "mask-url-router"
+    );
+  };
+
   return (
-    <div className={styles.container}>
-      <h1>메인페이지입니다.</h1>
-      <Link href="/test">테스트페이지로 이동</Link>
-    </div>
+    <>
+      <div>
+        <Header />
+        <h1>메인페이지입니다.</h1>
+        <Link href="/test" as="mask-url-link">
+          {`<Link>`}를 사용한 테스트페이지로 이동
+        </Link>
+
+        <p onClick={moveTestPage}>useRouter를 사용한 테스트페이지 이동</p>
+      </div>
+
+      {/* next만의 스타일 적용 방법 */}
+      <style jsx>{`
+        p {
+          cursor: pointer;
+          color: blue;
+        }
+      `}</style>
+    </>
   );
 }
